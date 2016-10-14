f461acff8829a9c3d2b3bffaffb7ca378017f79a
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-10-14 09:50:14 -0400

    Add connection to local mongo db

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
 
 var app = express();
 
 app.get('/', function(request, response) {
-  response.end('Hello World!');
+  MongoClient.connect("mongodb://localhost:27017/url_shortener", function(err, db) {
+    if(err) { console.log("There was the following error connecting to the database", err); }
+    response.end('You are connected to the database.');
+  });
 });
 
 app.listen(8080, function() {
