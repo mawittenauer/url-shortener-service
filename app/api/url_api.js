ddf096e6d87e186d7abd28b7443a96c3ac2bf8df
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-10-17 11:03:12 -0400

    Deactivate counter when url is not valid

--- app/api/url_api.js
+++ app/api/url_api.js
@@ -10,26 +10,28 @@ module.exports = function(app, db) {
     if(!request.body) return response.sendStatus(400);
     console.log(JSON.stringify(request.body));
     var url = request.body.url;
-    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
     var url_regex = new RegExp(expression);
 
-    if(!url.match(url_regex)) { response.end('That is not a valid url'); }
-
-    var counter_collection = db.collection('counter');
-    counter_collection.update( { "id" : "counter" }, { $inc: { count: 1 } }, function(err, data) {
-      if(err) { console.log("There was an error incrementing the count", err); }
-    });
-
-    counter_collection.findOne({ "id" : "counter" }, function(err, data) {
-      if(err) { console.log("There was an error finding count", err); }
-      var urls_collection = db.collection('urls');
-      var url_data = { url: url, url_id: data["count"] }
-      urls_collection.insert(url_data, function(err, doc) {
-        if(err) { console.log("There was an error inserting into mongodb", err); }
-        var return_data = { old_url: url, new_url: "localhost:8080/" + data["count"] }
-        response.end(JSON.stringify(return_data));
+    if(!url.match(url_regex)) { 
+      response.end('That is not a valid url');
+    } else {
+      var counter_collection = db.collection('counter');
+      counter_collection.update( { "id" : "counter" }, { $inc: { count: 1 } }, function(err, data) {
+        if(err) { console.log("There was an error incrementing the count", err); }
       });
-    });
+
+      counter_collection.findOne({ "id" : "counter" }, function(err, data) {
+        if(err) { console.log("There was an error finding count", err); }
+        var urls_collection = db.collection('urls');
+        var url_data = { url: url, url_id: data["count"] }
+        urls_collection.insert(url_data, function(err, doc) {
+          if(err) { console.log("There was an error inserting into mongodb", err); }
+          var return_data = { old_url: url, new_url: "localhost:8080/" + data["count"] }
+          response.end(JSON.stringify(return_data));
+        });
+      });
+    }
   });
   
   app.get('/:url_id', function(request, response) {
@@ -37,7 +39,7 @@ module.exports = function(app, db) {
     var url_collection = db.collection('urls');
     url_collection.findOne( { "url_id" : parseInt(url_id) }, function(err, data) {
       if(err) { console.log("There was an error finding that url", err); }
-      response.redirect("http://" + data["url"]);
+      response.redirect(data["url"]);
     });
   });
 }
