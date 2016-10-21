a40dd054c62f8ec436b734628f7e14413569e015
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-10-21 12:09:49 -0400

    Add Procfile and mlab url for herkou deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/assets'));
 
-MongoClient.connect("mongodb://localhost:27017/url_shortener", function(err, db) {
+MongoClient.connect(process.env.DATABASE_URL || "mongodb://localhost:27017/url_shortener", function(err, db) {
   if(err) { console.log("There was the following error connecting to the database", err); }
   
   app.get('/', function(request, response) {
